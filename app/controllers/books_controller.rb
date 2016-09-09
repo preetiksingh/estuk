f6e81ca255a7dcab99927805602662c61e5ede79
preetiksingh <preetisingh41451@gmail.com>
2016-09-09 13:44:25 +0530

    Added Book user association

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,65 +1,47 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  # GET /books
-  # GET /books.json
+respond_to :html
+
   def index
     @books = Book.where(availability: true)
+    respond_with(@books)
   end
 
-  # GET /books/1
-  # GET /books/1.json
   def show
+    respond_with(@book)
   end
 
-  # GET /books/new
+ 
   def new
     @book = Book.new
+    respond_with(@book)
   end
 
-  # GET /books/1/edit
+ 
   def edit
   end
 
-  # POST /books
-  # POST /books.json
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.new(book_params)
+    @book.save
+    respond_with(@book)
 
-    respond_to do |format|
-      if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
+   
   end
 
-  # PATCH/PUT /books/1
-  # PATCH/PUT /books/1.json
+ 
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
+   @book.update(book_params)
+   respond_with(@book)
   end
 
-  # DELETE /books/1
-  # DELETE /books/1.json
+ 
   def destroy
     @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
+    respond_with(@book)
     end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
